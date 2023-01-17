f2ad963c140b14dd64d016cf269393b55ccb8f6b
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-01-17 14:50:17 +0000

    chore(release): v5.0.0

--- helm/go.mod
+++ helm/go.mod
@@ -1,9 +1,9 @@
-module github.com/cdktf/cdktf-provider-helm-go/helm/v4
+module github.com/cdktf/cdktf-provider-helm-go/helm/v5
 
 go 1.18
 
 require (
-	github.com/aws/jsii-runtime-go v1.72.0
-	github.com/hashicorp/terraform-cdk-go/cdktf v0.14.3
-	github.com/aws/constructs-go/constructs/v10 v10.1.189
+	github.com/aws/jsii-runtime-go v1.73.0
+	github.com/hashicorp/terraform-cdk-go/cdktf v0.15.0
+	github.com/aws/constructs-go/constructs/v10 v10.1.215
 )
--- helm/provider/provider_HelmProvider.go
+++ helm/provider/provider_HelmProvider.go
@@ -2,10 +2,10 @@ package provider
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-helm-go/helm/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-helm-go/helm/v5/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-helm-go/helm/v4/provider/internal"
+	"github.com/cdktf/cdktf-provider-helm-go/helm/v5/provider/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
