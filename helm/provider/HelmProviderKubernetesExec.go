d867d65bd88ec9a5cbbfdda3145f5b07b9ac969c
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-04-17 03:13:25 +0000

    chore(release): v10.1.1

--- helm/provider/HelmProviderKubernetesExec.go
+++ helm/provider/HelmProviderKubernetesExec.go
@@ -5,13 +5,13 @@ package provider
 
 
 type HelmProviderKubernetesExec struct {
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.0/docs#api_version HelmProvider#api_version}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.1/docs#api_version HelmProvider#api_version}.
 	ApiVersion *string `field:"required" json:"apiVersion" yaml:"apiVersion"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.0/docs#command HelmProvider#command}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.1/docs#command HelmProvider#command}.
 	Command *string `field:"required" json:"command" yaml:"command"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.0/docs#args HelmProvider#args}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.1/docs#args HelmProvider#args}.
 	Args *[]*string `field:"optional" json:"args" yaml:"args"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.0/docs#env HelmProvider#env}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.1/docs#env HelmProvider#env}.
 	Env *map[string]*string `field:"optional" json:"env" yaml:"env"`
 }
 
--- helm/release/Release.go
+++ helm/release/Release.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
-// Represents a {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.0/docs/resources/release helm_release}.
+// Represents a {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.1/docs/resources/release helm_release}.
 type Release interface {
 	cdktf.TerraformResource
 	Atomic() interface{}
@@ -1277,7 +1277,7 @@ func (j *jsiiProxy_Release) WaitInput() interface{} {
 }
 
 
-// Create a new {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.0/docs/resources/release helm_release} Resource.
+// Create a new {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.1/docs/resources/release helm_release} Resource.
 func NewRelease(scope constructs.Construct, id *string, config *ReleaseConfig) Release {
 	_init_.Initialize()
 
@@ -1295,7 +1295,7 @@ func NewRelease(scope constructs.Construct, id *string, config *ReleaseConfig) R
 	return &j
 }
 
-// Create a new {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.0/docs/resources/release helm_release} Resource.
+// Create a new {@link https://registry.terraform.io/providers/hashicorp/helm/2.13.1/docs/resources/release helm_release} Resource.
 func NewRelease_Override(r Release, scope constructs.Construct, id *string, config *ReleaseConfig) {
 	_init_.Initialize()
 
